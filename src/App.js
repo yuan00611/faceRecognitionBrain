1a331b0e5c0fb6e498b67482de711cc633c20105
陳德元 <r06521608@ntu.edu.tw>
2020-06-03 15:24:37 -0500

    try to fix

--- src/App.js
+++ src/App.js
@@ -54,12 +54,6 @@ class App extends React.Component {
     }})
   }
 
-  // componentDidMount() {
-  //   fetch('http://localhost:3000/')
-  //     .then(response => response.json())
-  //     .then(console.log)
-  // }
-
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
